d7f5488aa8d3a8928e8cdd634ea7d7ae37f50d62
nichita <ursu@b2b.cash>
2018-11-13 18:08:54 +0200

    deploy 2

--- src/assets/config.ts
+++ src/assets/config.ts
@@ -1,4 +1,4 @@
-export const WEBSOCKET_CONNECT_URL = 'ws://89.223.27.70/ws';
+export const WEBSOCKET_CONNECT_URL = 'wss://89.223.27.70/ws';
 export const BASE_HREF = 'http://89.223.27.70';
 
 export const PARTICIPANTS_EVENT_ROUTE = '/topic/messages';
